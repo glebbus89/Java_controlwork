3b00eca32ebd377b95a50856fa4c46d9b032e6d7
Gleb <glebbus89@gmail.com>
2023-10-16 14:30:02 +0300

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        ToyCollection toys = new ToyCollection();
+        toys.putToy(new Toy("конструктор", 2));
+        toys.putToy(new Toy("робот", 2));
+        toys.putToy(new Toy("кукла", 6));
+
+        System.out.println("Добро пожаловать на розыгрыш призов в магазине игрушек!");
+
+        String menu = "Выберите действие:\n" +
+                "[1] - Разыграть игрушку,\n" +
+                "[2] - Посмотреть наличие игрушек в автомате,\n" +
+                "[3] - Посмотреть игрушки, готовые к выдаче (выигранные),\n" +
+                "[4] - Выдать одну игрушку,\n" +
+                "[5] - Посмотреть меню,\n" +
+                "[0] - Закончить работу в программе.\n";
+
+        System.out.println(menu);
+        Scanner input = new Scanner(System.in);
+        String action = "";
+
+        while (!action.equals("0")) {
+            System.out.print("\nВведите команду (5 - для вызова меню): ");
+            action = input.nextLine();
+            switch (action) {
+                case "1": // Разыграть игрушку
+                    System.out.print("Сколько раз разыграть призы? Напишите цифру: ");
+                    int steps = input.nextInt();
+                    input.nextLine();
+                    for (int i = 0; i < steps; i++) {
+                        System.out.println("\nЗапуск розыгрыша №" + (i + 1));
+                        toys.runLottery();
+                    }
+                    break;
+                case "2": // Посмотреть наличие игрушек в автомате
+                    System.out.println("\nВсего подарков в автомате:");
+                    toys.printActiveToyCol();
+                    break;
+                case "3": // Посмотреть игрушки, готовые к выдаче (выигранные)
+                    System.out.println("\nПодарки, готовые к выдаче:");
+                    toys.printReadyToGiveToyCol();
+                    break;
+                case "4": // Выдать игрушку
+                    System.out.println("\nВыдача призов");
+                    toys.getToys();
+                    break;
+                case "5": // Посмотреть меню
+                    System.out.println(menu);
+                    break;
+                case "0": // Закончить работу в программе
+                    System.out.println("\nДо свидания!");
+                    break;
+                default:
+                    System.out.println("\nТакой команды нет, введите цифру из меню.");
+                    break;
+            }
+        }
+
+        input.close();
+    }
+
+}
---
+++ src/ToyCollection.java
@@ -0,0 +1,103 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Random;
+
+
+public class ToyCollection {
+    protected LinkedList<Toy> collection = new LinkedList<>();
+    protected Queue<Prize> prizeQueue = new LinkedList<>();
+
+        public void putToy(Toy toy) {
+            this.collection.add(toy);
+            setAllChances();
+        }
+
+        public void setAllChances() {
+            int sum = 0;
+            for (Toy toy : collection) {
+                sum += toy.quantity;
+            }
+            for (Toy toy : collection) {
+                toy.setChance(toy.quantity * 100 / sum);
+            }
+        }
+
+        public void runLottery() {
+            if (collection.isEmpty()) {
+                System.out.println("Невозможно провести розыгрыш. В автомате нет игрушек.");
+            } else {
+                double possibility = new Random().nextInt(100);
+                double start = 0;
+                collection.sort(null);
+                for (Toy toy : collection) {
+                    if (possibility >= start && possibility < (start + toy.chance)) {
+                        prizeQueue.add(new Prize(toy.name));
+                        if (toy.quantity > 1)
+                            toy.setQuantity(toy.quantity - 1);
+                        else
+                            collection.remove(toy);
+                        System.out.printf("Участник выиграл игрушку: %s\n", toy.name);
+                    }
+                    start += toy.chance;
+                }
+                setAllChances();
+                collection.sort(null);
+            }
+        }
+
+        public void getToys() {
+            if (prizeQueue.isEmpty()) {
+                System.out.println("Нет игрушек, готовых к выдаче участникам. Нужно вначале провести розыгрыш призов.");
+            } else {
+                Date date = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("E yyyy.MM.dd hh:mm:ss");
+                String fileName = "PrizeReport.txt";
+                Prize prize = prizeQueue.element();
+                try (FileWriter writer = new FileWriter(fileName, Charset.forName("utf-8"), true)) {
+                    writer.write(dateFormat.format(date) + " ВЫДАН ПРИЗ: номер лота " + prize.getId() + ", игрушка - "
+                            + prize.name + "\n");
+                    writer.flush();
+                    System.out.println(dateFormat.format(date) + " ВЫДАН ПРИЗ: номер лота " + prize.getId() + ", игрушка - "
+                            + prize.name + ". Запись о выдаче внесена в файл.\n");
+                    prizeQueue.remove();
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
+
+        public void printActiveToyCol() {
+            if (collection.isEmpty()) {
+                System.out.println("Нет игрушек в автомате.");
+            } else {
+                for (Toy toy : collection) {
+                    System.out.println(toy);
+                }
+            }
+        }
+
+        public void printReadyToGiveToyCol() {
+            if (prizeQueue.isEmpty()) {
+                System.out.println("Нет игрушек, готовых к выдаче.");
+            } else {
+                Map<String, Integer> prizes = new HashMap<>();
+                ;
+                for (Prize prize : prizeQueue) {
+                    int count = 1;
+                    if (prizes.containsKey(prize.name))
+                        count = prizes.get(prize.name) + 1;
+                    prizes.put(prize.name, count);
+                }
+                for (var prize : prizes.entrySet()) {
+                    System.out.printf("- %s, количество %d\n", prize.getKey(), prize.getValue());
+                }
+            }
+        }
+}
